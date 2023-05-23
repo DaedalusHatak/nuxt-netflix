0a1b6f795a8bd61a6aaf335966f832a2853f55bc
DaedalusHatak <pawelkic55@gmail.com>
2023-05-23 19:23:35 +0200

    firebase env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['~/assets/main.css', '~/assets/base.css'],
-	// plugins: ['~/plugins/firebase.ts'],
-	modules: ['@pinia/nuxt','@pinia-plugin-persistedstate/nuxt'],
+  css: ["~/assets/main.css", "~/assets/base.css"],
+  // plugins: ['~/plugins/firebase.ts'],
+  modules: ["@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
 
-	runtimeConfig: {
-		apiSecret: '',
-		apiLink: '',
-		imgLink: '',
-		public: {
-			firebase: {
-				apiKey: '',
-				authDomain: '',
-				projectId: '',
-				storageBucket: '',
-				messagingSenderId: '',
-				appId: '',
-				measurementId: '',
-				
-			},
-			apiBase: '/api',
-		},
-	},
+  runtimeConfig: {
+    apiSecret: "",
+    apiLink: "",
+    imgLink: "",
+    firebase: {
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: "",
+    },
+    public: {
+      firebase: {
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: "",
+      },
+      apiBase: "/api",
+    },
+  },
 });
