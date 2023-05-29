c79fb5b536f68d8341f26f4eeb6b913434cb4006
DaedalusHatak <pawelkic55@gmail.com>
2023-05-29 16:25:58 +0200

    added route guard at onAuthState

--- composables/getFirebase.ts
+++ composables/getFirebase.ts
@@ -1,65 +1,70 @@
 import {
-	getAuth,
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	onAuthStateChanged,
-	signOut,
-} from 'firebase/auth';
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 
 export const createUser = async (email: string, password: string) => {
-	const auth = getAuth();
-	const credentials = await createUserWithEmailAndPassword(
-		auth,
-		email,
-		password
-	);
-	navigateTo('/browse');
-	return credentials;
+  const auth = getAuth();
+  const credentials = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  navigateTo("/browse");
+  return credentials;
 };
 export const signIn = async (email: string, password: string) => {
-	const auth = getAuth();
-	const credentials = await signInWithEmailAndPassword(auth, email, password);
+  const auth = getAuth();
+  const credentials = await signInWithEmailAndPassword(auth, email, password);
 
-	navigateTo('/browse');
-	return credentials;
+  navigateTo("/browse");
+  return credentials;
 };
 
-export const initUser =  () => {
-	const auth = getAuth();
+export const initUser = () => {
+  const auth = getAuth();
 
-	const scrfToken = useCookie('token');
-	onAuthStateChanged(auth, async (user) => {
-		if (user) {
-			const tok = await user.getIdToken();
-			let response;
-	
-			if (scrfToken.value === tok) {
-				await useFetch('/api/keepLogged', {
-					method: 'POST',
-					body: { token: scrfToken, isToken: true },
-				})
-     
-			} else {
-        console.log('uyser else')
-				await useFetch("/api/keepLogged", { method: "POST", body: {token:null,isToken:false } })
-       
-				
-	
-			}
-		} 
-    
-    else {
-		}
-	});
+  const scrfToken = useCookie("token");
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      const tok = await user.getIdToken();
+      let response;
+
+      if (scrfToken.value === tok) {
+        await useFetch("/api/keepLogged", {
+          method: "POST",
+          body: { token: scrfToken, isToken: true },
+        });
+      } else {
+        console.log("uyser else");
+        await useFetch("/api/keepLogged", {
+          method: "POST",
+          body: { token: null, isToken: false },
+        });
+        const router = useRoute();
+        console.log(router.path);
+        if (router.path === "/browse") {
+          navigateTo("/login");
+        }
+      }
+    } else {
+    }
+  });
 };
 
 export const signOutUser = async () => {
-	const auth = getAuth();
-	const result = await auth.signOut();
-	const authToken = null;
-	const scrfToken = useCookie('token');
-	await useFetch('/api/auth', { method: 'POST', body: { scrfToken, authToken } });
+  const auth = getAuth();
+  const result = await auth.signOut();
+  const authToken = null;
+  const scrfToken = useCookie("token");
+  await useFetch("/api/auth", {
+    method: "POST",
+    body: { scrfToken, authToken },
+  });
 
-	navigateTo('/');
-	return result;
+  navigateTo("/");
+  return result;
 };
