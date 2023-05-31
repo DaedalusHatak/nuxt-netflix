14816477a015398084b011ae0406d43f76543656
DaedalusHatak <pawelkic55@gmail.com>
2023-05-31 13:38:31 +0200

    keepEmail cookie

---
+++ server/api/keepEmail.ts
@@ -0,0 +1,18 @@
+export default defineEventHandler(async (event) => {
+    const {email,isToken } = await readBody(event);
+    const cookieOptions = {
+
+        expires: new Date(Date.now() + 1 * 1000)
+    }
+   if(isToken){
+    const baba = await setCookie(event,'signUp',email,cookieOptions);
+
+   }
+   else{
+    console.log("else token")
+    const baba = await setCookie(event,'signUp',email);
+   
+   }
+   return isToken
+  });
+  
