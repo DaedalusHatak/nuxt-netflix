69af1d4840b04cc9af67fe74f62eee5ab181d1dd
DaedalusHatak <pawelkic55@gmail.com>
2023-08-21 10:59:56 +0200

    check build

--- server/api/session.ts
+++ server/api/session.ts
@@ -2,19 +2,19 @@
 import { H3Event,useSession,updateSession } from "h3";
 
 export default defineEventHandler(async (event: H3Event) => {
-    const {email} = await readBody(event);
-    let session;
+    // const {email} = await readBody(event);
+    // let session;
 
-    if(email){
+    // if(email){
        
-        session = await updateSession(event,{password:''},{email:email})
-        console.log('if',session)
+    //     session = await updateSession(event,{password:''},{email:email})
+    //     console.log('if',session)
 
-    }
-    else{
-        session = await useSession(event,{password:''})
-        console.log('else',session)
+    // }
+    // else{
+    //     session = await useSession(event,{password:''})
+    //     console.log('else',session)
         
-    }
+    // }
     return true
 })
