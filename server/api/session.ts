f79a66401a2989e4b800c5abb0e38a3f648d16ad
DaedalusHatak <pawelkic55@gmail.com>
2023-08-21 10:32:02 +0200

    fix: config

--- server/api/session.ts
+++ server/api/session.ts
@@ -1,8 +1,20 @@
 
-import { H3Event,useSession,SessionConfig } from "h3";
+import { H3Event } from "h3";
 
 export default defineEventHandler(async (event: H3Event) => {
     const {email} = await readBody(event);
-    const session = await useSession(event,email)
-return session
+    let session;
+
+    if(email){
+       
+        session = await updateSession(event,{password:''},{email:email})
+        console.log('if',session)
+        return session
+    }
+    else{
+        session = await useSession(event,{password:''})
+        console.log('else',session)
+        return session
+    }
+
 })
