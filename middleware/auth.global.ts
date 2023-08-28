d03575583f94042c66064d055b079fbc351973c3
DaedalusHatak <pawelkic55@gmail.com>
2023-08-28 22:28:05 +0200

    fix: auth target

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,26 +1,26 @@
-import { userInfo } from "os";
 interface APISession {
-  photoURL: string,
-  email: string,
+  photoURL: string;
+  email: string;
 }
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const test = useCookie("__token");
+
   const { data } = await useFetch<APISession>("/api/checkSession", {
     method: "POST",
     body: { test: test.value },
   });
-  const userInfo = useProfile()
-if(data.value){
-  userInfo.value = {photo: data.value!.photoURL, email: data.value!.email};
-}
+  const userInfo = useProfile();
+  if (data.value) {
+    userInfo.value = { photo: data.value!.photoURL, email: data.value!.email };
+  }
   if (data.value) {
     if (to.path === "/YourAccount") {
       return;
-    } else if (!to.path.startsWith("/browse")) {
+    } else if (to.matched[0].path !== "/browse") {
       return navigateTo("/browse");
     }
-  } else if (!data.value && to.path.startsWith("/browse")) {
+  } else if (!data.value && to.matched[0].path === "browse") {
     return navigateTo("/login");
   }
 });
