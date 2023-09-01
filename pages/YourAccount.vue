fb34ed1bdc624fc9cebda734b8d524dc86a85e2b
DaedalusHatak <pawelkic55@gmail.com>
2023-09-01 14:00:48 +0200

    fix: phone change

--- pages/YourAccount.vue
+++ pages/YourAccount.vue
@@ -1,214 +1,349 @@
 <script setup lang="ts">
-import { PhoneAuthProvider, RecaptchaVerifier, getAuth, updatePhoneNumber } from 'firebase/auth';
+import {
+	PhoneAuthProvider,
+	RecaptchaVerifier,
+	getAuth,
+	linkWithPhoneNumber,
+	signInWithPhoneNumber,
+	updatePhoneNumber,
+} from 'firebase/auth';
 
 const userProfile = useProfile();
-console.log((userProfile))
 const modalMessage = ref();
 const firestoreClient = ref({
-  avatar:  userProfile.value.photoURL || "raiden.png",
-  email: userProfile.value.email,
-  phone: userProfile.value.providerData[0].phoneNumber ? userProfile.value.providerData[0].phoneNumber : ''
+	avatar: userProfile.value.photoURL || 'raiden.png',
+	email: userProfile.value.email,
+	phone: userProfile.value.providerData[0].phoneNumber
+		? userProfile.value.providerData[0].phoneNumber
+		: '',
 });
 const isVeryfing = ref(false);
+const buttonCaptcha = ref(false);
 const changePhone = ref(false);
-const newNumber = ref<string>("");
-let verificationId:any ;
-let verificationCode:any;
-let applicationVerifier:any;
-async function updatePhone(number:string){
-  const auth = getAuth();
-  auth.useDeviceLanguage();
-try{
-   applicationVerifier = new RecaptchaVerifier(auth,'recap', {
-  'size':'invisible',
-  'callback': (response:any) => {
+const newNumber = ref<string>('');
+const verCode = ref();
 
-  }})
-  
-  const provider = new PhoneAuthProvider(auth);
-  verificationId = await provider.verifyPhoneNumber(number, applicationVerifier);
-  console.log((verificationId))
-  modalMessage.value = "Enter verification code"
-  isVeryfing.value = true;
+let verificationId: any;
+let verificationCode: any;
+let applicationVerifier: any;
+let currentPhone: any;
+
+async function updatePhone(number: string) {
+
+	buttonCaptcha.value = true;
+	const auth = getAuth();
+
+	if (!applicationVerifier) {
+		applicationVerifier = new RecaptchaVerifier(auth, 'recap', {
+			size: 'invisible',
+			callback: (response: any) => {
+				buttonCaptcha.value = false;
+			},
+		});
+	}
+	try {
+		const provider = new PhoneAuthProvider(auth);
+		verificationId = await provider.verifyPhoneNumber(
+			number,
+			applicationVerifier
+		);
+		currentPhone = await signInWithPhoneNumber(
+			auth,
+			number,
+			applicationVerifier
+		);
+		modalMessage.value = 'Enter verification code';
+		isVeryfing.value = true;
+	} catch (err) {
+		console.log(err);
+		modalMessage.value = 'Too many requests!';
+		isVeryfing.value = true;
+	}
 }
-catch(err){
-  modalMessage.value = "Too many requests!"
-  isVeryfing.value = true;
+
+async function verifyCode(verificationCode: any) {
+	isVeryfing.value = false;
+	if (!newNumber.value) {
+		await currentPhone.confirm(verificationCode);
+		changePhone.value = true;
+	} else {
+		if (verificationCode) {
+			verCode.value = verificationCode;
+			sendPhone();
+		}
+	}
 }
+async function verifyPhone(number: any) {
+	changePhone.value = false;
+	const auth = getAuth();
+	const provider = new PhoneAuthProvider(auth);
+	if (!applicationVerifier) {
+		applicationVerifier = new RecaptchaVerifier(auth, 'recap', {
+			size: 'invisible',
+			callback: (response: any) => {
+      },
+      'expired-callback': () => {
+  }
+		});
+    
+	}
+  buttonCaptcha.value = false;
+	verificationId = await provider.verifyPhoneNumber(
+		number,
+		applicationVerifier
+	);
 
-
-
-
-
- 
+	isVeryfing.value = true;
 }
-function verifyCode(verificationCode:any){
-  isVeryfing.value = false;
-  if(verificationCode)
-  {changePhone.value = true;}
-}
-
-async function sendPhone(verificationCode:any ){
-  changePhone.value = false;
- if(verificationCode){
-  const auth = getAuth();
- console.log("id",verificationId)
- console.log("Code",verificationCode)
-await updatePhone(newNumber.value)
- const phoneCredential = await PhoneAuthProvider.credential(verificationId, verificationCode);
- 
-await updatePhoneNumber(auth.currentUser!, phoneCredential);
- }
-
+async function sendPhone() {
+	changePhone.value = false;
+	const auth = getAuth();
+	try {
+		const phoneCredential = await PhoneAuthProvider.credential(
+			verificationId,
+			verCode.value
+		);
+		await updatePhoneNumber(auth.currentUser!, phoneCredential);
+    firestoreClient.value.phone = newNumber.value;
+	} catch (err) {
+		console.log(err);
+	}
 }
 </script>
 
 <template>
-<div id="recap"></div>
- <BaseModal v-if="isVeryfing" @closeModal="verifyCode" :message="modalMessage"></BaseModal>
- <div v-if="changePhone"  class="modal-backdrop" style="">
-  <form @submit.prevent="sendPhone(newNumber)" action="">
-    <div class="modal">
-      <header class="modal-header">Change Phone Number</header>
-     
-      <section class="modal-body">
-      Enter new phone number
-     
-        <input v-model="newNumber" type="text">
-    
-      </section>
-    
-      <footer class="modal-footer">
-        <button @click="sendPhone" type="button" class="btn-green">
-          Confirm
-        </button>
-        <button @click="changePhone = false"  type="button" class="btn-green">
-          Cancel
-        </button>
-      </footer>
-    
-    </div>
-  </form>
-  </div>
- <NavBar :isAccount="true" :av="firestoreClient"></NavBar>
- <div class="flex-center">
-  <h1>Account</h1>
-  <div class="account">
-   <div> Membership and billing</div>
-   <div class="details">
-<div class="detail-item">
-  <p>{{ firestoreClient.email }}</p>
-    <button>Change your email address</button>
-</div>
-<div class="detail-item">
-  <p>Password: ********</p>
-    <button>Change password</button>
-</div>
+	<div id="recap"></div>
+	<BaseModal
+		v-if="isVeryfing"
+		@closeModal="verifyCode"
+		:message="modalMessage"
+	></BaseModal>
+	<div v-if="changePhone" class="modal-backdrop" style="">
+		<form @submit.prevent="verifyPhone(newNumber)" action="">
+			<div class="modal">
+				<header class="modal-header">Change Phone Number</header>
 
-<div class="detail-item">
-  <p>{{ firestoreClient.phone }}</p>
-    <button id="recaptcha-container" @click="updatePhone('+48665123514')">Change phone number</button>
-    <form @submit.prevent="sendPhone(verificationCode)">
-      <input   v-model="verificationCode" type="text">
-      <button >Submit form</button>
-    </form>
-</div>
-   </div>
-   
-  </div>
- </div>
+				<section class="modal-body">
+					Enter new phone number
+
+					<input v-model="newNumber" type="text" />
+				</section>
+
+				<footer class="modal-footer">
+					<button
+						@click="verifyPhone(newNumber)"
+						type="button"
+						class="btn-green"
+					>
+						Confirm
+					</button>
+					<button @click="changePhone = false" type="button" class="btn-green">
+						Cancel
+					</button>
+				</footer>
+			</div>
+		</form>
+	</div>
+	<NavBar :isAccount="true" :av="firestoreClient"></NavBar>
+	<div class="flex-center">
+		<h1>Account</h1>
+		<div class="account">
+			<div> Membership and billing</div>
+			<div class="details">
+				<div class="detail-item">
+					<p>{{ firestoreClient.email }}</p>
+					<button class="change-button">Change your email address</button>
+				</div>
+				<div class="detail-item">
+					<p>Password: ********</p>
+					<button class="change-button">Change password</button>
+				</div>
+
+				<div class="detail-item">
+					<p>{{ firestoreClient.phone }}</p>
+					<div>
+						<button
+							id="recaptcha-container"
+							@click="updatePhone(firestoreClient.phone)"
+              :class="buttonCaptcha ? 'button-spinner' : ''"
+							class="change-button"
+						>
+							<span v-if="!buttonCaptcha">Change phone number</span>
+							<div v-else class="loader">
+								<span class="loader-circle"></span>
+							</div>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
-h1{
-  font-size: 1.5em;
-  text-align: left;
+h1 {
+	font-size: 1.5em;
+	text-align: left;
 }
-.account{
-  display:grid;
-  grid-template-columns: 300px 2fr;
-  border-top: 1px solid #999;
+p{
+  font-size: 17px;
 }
-.details{
-  display:grid;
-  grid-template-columns: 1fr;
-  justify-content: space-between;
+.account {
+	display: grid;
+	grid-template-columns: 300px 2fr;
+	border-top: 1px solid #999;
 }
-.detail-item{
-  display:flex;
-  justify-content: space-between;
+.details {
+	display: grid;
+	grid-template-columns: 1fr;
+	justify-content: space-between;
+}
+.detail-item {
+	display: flex;
+	margin-top: 1rem;
+	justify-content: space-between;
+	align-items: center;
+}
+.change-button {
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	writing-mode: horizontal-tb !important;
+
+
+	flex: 0 0 auto;
+	border: 0px;
+
+	font-size: clamp(0.5rem, 1.5vw, 1rem);
+	font-weight: 500;
+	min-height: 2.5rem;
+	padding: 0.75rem 1rem;
+	background: rgb(51, 26, 187, 0.9);
+	cursor: pointer;
+	color: rgb(255, 255, 255);
+	border-radius: 1rem;
+}
+.button-spinner{
+  min-width: 200px;
 }
 .flex-center {
-  text-align: center;
-  padding: 2rem 1rem;
-  display: flex;
-  width:90%;
-  margin: 0 auto;
-  flex-direction: column;
-  overflow: hidden;
+	text-align: center;
+	padding: 2rem 1rem;
+	display: flex;
+	width: 90%;
+	margin: 0 auto;
+	flex-direction: column;
+	overflow: hidden;
 }
 
 .modal-backdrop {
-  z-index: 999;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: #0000004d;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	z-index: 999;
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background-color: #0000004d;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 
 .modal {
-  width: calc(100% - 4rem);
-  z-index: 999;
-  background: #ffffff;
-  border-radius: 15px;
-  overflow-x: auto;
-  display: flex;
-  flex-direction: column;
+	width: calc(100% - 4rem);
+	z-index: 999;
+	background: #ffffff;
+	border-radius: 15px;
+	overflow-x: auto;
+	display: flex;
+	flex-direction: column;
 }
 
 .modal-header,
 .modal-body,
 .modal-footer {
-  padding: 15px;
-  display: flex;
+	padding: 15px;
+	display: flex;
 
-  gap: 1.25rem;
-  justify-content: flex-end;
+	gap: 1.25rem;
+	justify-content: flex-end;
 }
 .modal-header {
-  font-size: 20px;
+	font-size: 20px;
 }
 .modal-body {
-  flex-direction: column;
+	flex-direction: column;
 }
 
 .modal-header,
 .modal-body {
-  border-bottom: 1px solid #eeeeee;
-  color: #242424;
-  justify-content: space-between;
+	border-bottom: 1px solid #eeeeee;
+	color: #242424;
+	justify-content: space-between;
 }
 .error {
-  color: red;
+	color: red;
 }
 
 .btn-green {
-  color: #fff;
-  cursor: pointer;
+	color: #fff;
+	cursor: pointer;
 
-  padding: 8px;
-  background: rgba(51, 26, 187, 0.9);
-  border: 1px solid rgba(51, 26, 187, 0.9);
-  border-radius: 8px;
+	padding: 8px;
+	background: rgba(51, 26, 187, 0.9);
+	border: 1px solid rgba(51, 26, 187, 0.9);
+	border-radius: 8px;
+}
+
+.loader {
+	border: 11px solid #f3f3f3; /* Light grey */
+	border-top: 11px solid #3498db; /* Blue */
+	border-radius: 100%;
+	position: relative;
+	height: 100%;
+  max-height: 20px;
+	animation: spin 2s linear infinite;
+}
+.loader span {
+	background-color: rgb(51, 26, 187);
+	display: block;
+	position: absolute;
+	inset: 0;
+	left: -12px;
+	top: -12px;
+	border-radius: 100%;
+	height: 24px;
+	width: 24px;
+
+	transform: scale(0.75, 0.75);
+}
+@keyframes spin {
+	0% {
+		transform: rotate(0deg);
+	}
+	100% {
+		transform: rotate(360deg);
+	}
 }
 @media screen and (min-width: 420px) {
-  .modal {
-    width: 420px;
+	.modal {
+		width: 420px;
+	}
+}
+
+@media screen and (max-width: 740px) {
+  .account{
+    grid-template-columns: auto;
+    grid-template-rows: max-content 1fr;
   }
+	.detail-item{
+    display: block;
+    width:100%;
+
+  }
+  .change-button{
+    width:100%;
+    font-size: 17px;
+}
 }
 </style>
